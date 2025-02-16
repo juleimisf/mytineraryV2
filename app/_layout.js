836bdc3458388a6e1045eeb105fc936031e7218d
juleimisf <juleimis1314@gmail.com>
2025-02-16 18:41:16 -0300

    finish work

--- app/_layout.js
+++ app/_layout.js
@@ -2,16 +2,17 @@ import { Stack } from 'expo-router';
 import { STRINGS } from '../components/utils/strings';
 
 export default function RootLayout() {
+
   return (
     <Stack>
       <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
       <Stack.Screen 
         name="cities/[name]" 
-        options={({ route }) => ({ 
-          title: route.params?.name || STRINGS.DETAIL_CITY_TOOLBAR,
+        options={{ 
+          title: STRINGS.DETAIL_CITY_TOOLBAR,
           headerBackTitleVisible: false,
           headerBackTitle: STRINGS.BACK_OPTION,
-        })} 
+        }} 
       />
     </Stack>
   );
--- app/cities/[name].js
+++ app/cities/[name].js
@@ -1,11 +1,20 @@
 import { View, Text, StyleSheet, Button } from "react-native";
-import { useLocalSearchParams , useRouter } from "expo-router";
+import { useLocalSearchParams , useNavigation, useRouter } from "expo-router";
 import { Link } from "expo-router";
 import { STRINGS } from "../../components/utils/strings";
+import { useEffect } from "react";
 
 export default function CityDetails() {
   const { name } = useLocalSearchParams(); 
+  const navigation = useNavigation()
 
+  navigation.setOptions({title})
+
+  useEffect(()=> {
+    navigation.setOptions({title : "hola"})
+  }
+  
+  )
   return (
   <View style={styles.container}>
       <Text style={styles.title}>{name}</Text>
