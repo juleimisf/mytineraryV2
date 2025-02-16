ccc8495411bd1e2772ef8d4c10fa86ec401f0bfa
juleimisf <juleimis1314@gmail.com>
2025-02-16 18:43:11 -0300

    finish work

--- app/cities/[name].js
+++ app/cities/[name].js
@@ -1,20 +1,11 @@
 import { View, Text, StyleSheet, Button } from "react-native";
 import { useLocalSearchParams , useNavigation, useRouter } from "expo-router";
-import { Link } from "expo-router";
 import { STRINGS } from "../../components/utils/strings";
-import { useEffect } from "react";
 
 export default function CityDetails() {
   const { name } = useLocalSearchParams(); 
   const navigation = useNavigation()
 
-  navigation.setOptions({title})
-
-  useEffect(()=> {
-    navigation.setOptions({title : "hola"})
-  }
-  
-  )
   return (
   <View style={styles.container}>
       <Text style={styles.title}>{name}</Text>
